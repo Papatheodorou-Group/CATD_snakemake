d6f7d3e970ecc9c03072a6f1885a4f8db8913ed7
zgr2788 <ozgur.beker@gmail.com>
2022-07-15 14:58:29 +0100

    Impelemented cell split function

--- Modules/Convert_split/split.R
+++ Modules/Convert_split/split.R
@@ -2,4 +2,21 @@
 ##
 ## @zgr2788
 
-filename <- sub("Scratch/Convert_split/", "", commandArgs(trailingOnly = TRUE))
+library(Seurat)
+set.seed(42) #For reproducability purposes
+
+partitionProp <- 0.5 #Can be edited later
+
+filename <- commandArgs(trailingOnly = TRUE)
+seuratObj <- readRDS(filename)
+
+splitSize <- floor(partitionProp * nrow(seuratObj@meta.data)) #Get the split size
+idxList <- seq_len(nrow(seuratObj@meta.data)) #List of all indices
+
+#Assign split group to each cell randomly and split
+seuratObj@meta.data$splitGroup <- as.integer(idxList %in% sample(idxList, size = splitSize))
+seuratSplit <- SplitObject(seuratObj, split.by = 'splitGroup')
+
+#Save the splits into outputs
+saveRDS(seuratSplit[[1]], file = sub(".rds", "_gen.rds", filename)) #Sent to pseudobulk generation workflow
+saveRDS(seuratSplit[[2]], file = sub(".rds", "_C0.rds", filename)) #Sent to ref/marker generation workflow
