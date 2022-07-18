d509c27ee10af38e4a3f7c5592dc7c4f895bf5f6
zgr2788 <ozgur.beker@gmail.com>
2022-07-18 13:23:39 +0100

    Added non-seeded splitting, separated seeded/unseeded runs'

--- Modules/Convert_split/split_seeded.R
+++ Modules/Convert_split/split_seeded.R
@@ -3,7 +3,7 @@
 ## @zgr2788
 
 library(Seurat)
-#set.seed(42) #For reproducability purposes
+set.seed(42) #For reproducability purposes
 
 partitionProp <- 0.5 #Can be edited later
 
@@ -19,5 +19,5 @@ seuratObj@meta.data$splitGroup <- as.integer(idxList %in% sample(idxList, size =
 seuratSplit <- SplitObject(seuratObj, split.by = 'splitGroup')
 
 #Save the splits into outputs
-saveRDS(seuratSplit[[1]], file = sub("_seurat.rds", "_gen.rds", filename)) #Sent to pseudobulk generation workflow
-saveRDS(seuratSplit[[2]], file = sub("_seurat.rds", "_C0.rds", filename)) #Sent to ref/marker generation workflow
+saveRDS(seuratSplit[[1]], file = sub("_seurat.rds", "_gen_seeded.rds", filename)) #Sent to pseudobulk generation workflow
+saveRDS(seuratSplit[[2]], file = sub("_seurat.rds", "_C0_seeded.rds", filename)) #Sent to ref/marker generation workflow
--- Snakefile
+++ Snakefile
@@ -3,5 +3,5 @@ include: "Modules/Psuedobulk/Snakefile"
 
 rule all:
     input:
-        "Input/Psuedobulks/sciPlex3_MCF7_Targets45toEND_and_vehicle_pbulks_seeded.rds",
-        "Input/Psuedobulks/sciPlex3_MCF7_Targets45toEND_and_vehicle_props_seeded.rds"
+        "Input/Psuedobulks/sciPlex3_MCF7_Targets45toEND_and_vehicle_pbulks.rds",
+        "Input/Psuedobulks/sciPlex3_MCF7_Targets45toEND_and_vehicle_props.rds"
