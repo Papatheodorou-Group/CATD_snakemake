4d5f8e4764f0876c073dcf80c78c58ddc462fc80
zgr2788 <ozgur.beker@gmail.com>
2022-07-21 10:23:53 +0100

    c_gen: added intersection of different tests & config entries

--- Modules/C_generation/genRef_2.R
+++ Modules/C_generation/genRef_2.R
@@ -3,17 +3,35 @@
 ## @zgr2788
 
 library(Seurat)
+library(future)
+library(MAST)
+library(DESeq2)
+
+plan('multisession', workers = 5) #Paralellism
 
 #Read data
-filename <- commandArgs(trailingOnly = TRUE)
+ args <- commandArgs(trailingOnly = TRUE)
+filename <- args[1]
+test_1 <- args[2]
+test_2 <- args[3]
 C_0 <- readRDS(filename)
 filename <- sub("Input/Cell_splits", "Input/References", filename)
+
+message(paste0('Running DGE with tests: ', test_1, ' & ', test_2))
 
 #Port cell types to Idents
 Idents(C_0) <- C_0@meta.data$cellType
 
 #Find cell type specific markers
-markers <- FindAllMarkers(C_0, min.pct = 0.5, logfc.threshold = log(2), test.use = "wilcox")
+markers_t1 <- FindAllMarkers(C_0, min.pct = 0.5, logfc.threshold = log(2), test.use = test_1)
+markers_t2 <- FindAllMarkers(C_0, min.pct = 0.5, logfc.threshold = log(2), test.use = test_2)
+
+#Filter markers by corrected p-val
+markers_t1 <- markers_t1[markers_t1$p_val_adj <= 0.05,]
+markers_t2 <- markers_t2[markers_t2$p_val_adj <= 0.05,]
+
+#Select those in both
+markers <- markers_t1[rownames(markers_t1) %in% rownames(markers_t2),]
 
 #Select cols
 markers <- markers[,c("avg_log2FC", "cluster", "gene")]
