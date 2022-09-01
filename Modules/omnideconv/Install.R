db405fc5d644b98b6023d849f84e0871153d954a
zgr2788 <ozgur.beker@gmail.com>
2022-09-01 01:02:37 +0100

    Added omnideconv + results summary script

---
+++ Modules/omnideconv/Install.R
@@ -0,0 +1,19 @@
+# Omnideconv installation script
+#
+# @zgr2788
+
+if (!require("pak", quietly = TRUE)){
+  install.packages("pak", repos="http://cran.us.r-project.org")
+}
+
+
+# complete installation, including Python dependencies
+suppressMessages(library(pak))
+pak::pkg_install("omnideconv/omnideconv", dependencies = TRUE)
+omnideconv::install_all_python()
+
+suppressMessages(library(omnideconv))
+print("Installed omnideconv successfully!")
+
+#Manual
+#mamba install r-base geos r-gert r-vctrs "gdal=3.3.3.""
---
+++ Modules/omnideconv/MOMF_run.R
@@ -0,0 +1,36 @@
+## MOMF deconv script
+##
+## @zgr2788
+
+
+#Load MOMF
+suppressMessages(library(omnideconv))
+
+#Get args and load files
+args <- commandArgs(trailingOnly = TRUE)
+filename_T <- args[1]
+filename_P <- args[2]
+filename_C0 <- args[3]
+filename_phenData <- args[4]
+filename_O <- args[5]
+
+T <- readRDS(filename_T)
+P <- readRDS(filename_P)
+C0 <- readRDS(filename_C0)
+phenData <- readRDS(filename_phenData)
+
+
+#Match genes in rows for both references
+common <- intersect(rownames(C0), rownames(T))
+T <- T[common,]
+C0 <- C0[common,]
+rm(common)
+
+#Build sig
+cellTypes <- phenData$cellType
+model <- omnideconv::build_model(C0, cellTypes, bulk_gene_expression = T, method = "momf")
+res <- t(omnideconv::deconvolute(T, signature = model, single_cell_object = C0, method = "momf"))
+res <- res[order(match(rownames(res), rownames(P))),]
+
+#Save and exit
+saveRDS(res, file=filename_O)
