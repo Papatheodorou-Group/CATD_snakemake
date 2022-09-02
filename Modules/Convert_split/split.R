228885bca52629b7ee6f577f2d265b928b8663b4
zgr2788 <ozgur.beker@gmail.com>
2022-09-02 13:45:13 +0100

    Added threshold for cell splits + updated c_gen mem limit

--- Modules/Convert_split/split.R
+++ Modules/Convert_split/split.R
@@ -14,10 +14,18 @@ filename <- sub("Input", "Input/Cell_splits", filename)
 splitSize <- floor(partitionProp * nrow(seuratObj@meta.data)) #Get the split size
 idxList <- seq_len(nrow(seuratObj@meta.data)) #List of all indices
 
+#Define low cell type counts
+riskGroup <- names(table(seuratObj@meta.data$cellType)[table(seuratObj@meta.data$cellType) < 10])
+
 #Assign split group to each cell randomly and split
 seuratObj@meta.data$splitGroup <- as.integer(idxList %in% sample(idxList, size = splitSize))
-seuratSplit <- SplitObject(seuratObj, split.by = 'splitGroup')
+seuratObj@meta.data$riskTag <- seuratObj@meta.data$cellType %in% riskGroup
+
+splitRef <- seuratObj[,(seuratObj@meta.data$splitGroup == 1 | seuratObj@meta.data$riskTag == TRUE)]
+splitGen <- seuratObj[,(seuratObj@meta.data$splitGroup == 0 | seuratObj@meta.data$riskTag == TRUE)]
+#Check to see if all cells are sampled in one of the groups!
+
 
 #Save the splits into outputs
-saveRDS(seuratSplit[[1]], file = sub("_seurat.rds", "_gen.rds", filename)) #Sent to pseudobulk generation workflow
-saveRDS(seuratSplit[[2]], file = sub("_seurat.rds", "_C0.rds", filename)) #Sent to ref/marker generation workflow
+saveRDS(splitGen, file = sub("_seurat.rds", "_gen.rds", filename)) #Sent to pseudobulk generation workflow
+saveRDS(splitRef, file = sub("_seurat.rds", "_C0.rds", filename)) #Sent to ref/marker generation workflow
