b7db1ca7fa6fa3d2e0efa6492db3366b40b097ef
zgr2788 <ozgur.beker@gmail.com>
2022-09-07 14:07:41 +0100

    Added basic heatmap for agreement between methods

---
+++ Modules/Res_explore/Heatmap_agr.R
@@ -0,0 +1,45 @@
+# Heatmap for method agreements
+#
+# @zgr2788
+
+suppressMessages(library(dplyr))
+suppressMessages(library(lsa))
+suppressMessages(library(viridis))
+
+
+
+sampleName <- commandArgs(trailingOnly = TRUE)
+
+
+getAvgDist <- function(x,y){
+  idx <- seq_len(ncol(x))
+  distances <- sapply(idx, function(z) cosine(x[,z], y[,z]))
+  return(mean(distances))
+}
+
+
+#Get files as list
+filenames <- list.files("Output") %>% lapply(., function(x) { x <- paste0("Output/", x) }) %>% lapply(., function(x) { grep(sampleName, x, value = TRUE) }) %>% .[lengths(.)!=0] %>% as.character(.)
+files <- lapply(filenames, readRDS)
+names(files) <- lapply(filenames, function(x) gsub("Output/.*res_(.*).rds", "\\1", x))
+
+#Generate combs to iterate over
+combs <- combn(files, 2)
+colnames(combs) <- combn(names(files), 2, paste0, collapse="@")
+avgDists <- sapply(seq_len(ncol(combs)), function(z) getAvgDist(combs[,z][[1]], combs[,z][[2]]))
+names(avgDists) <- colnames(combs)
+
+#Generate matrix
+mat <- matrix(0, nrow = length(files), ncol = length(files))
+diag(mat) <- 1
+colnames(mat) <- names(files)
+rownames(mat) <- names(files)
+
+#Copy upper triangle to lower
+mat[upper.tri(mat)] <- avgDists
+mat[lower.tri(mat)] <- t(mat)[lower.tri(mat)]
+
+#Save plot
+png(filename = paste0("Plots/",sampleName, "_metricsHeatmap.png"), width = 1920*3, height = 1080*3, res=300)
+heatmap(mat, col = rev(viridis(256)), scale = "none")
+dev.off()
