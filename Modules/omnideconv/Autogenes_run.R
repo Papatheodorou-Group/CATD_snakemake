72fcfa52421160e9dffc6e29eb11fd53717b74e1
zgr2788 <ozgur.beker@gmail.com>
2022-09-12 15:07:54 +0100

    Added error tolerance to autogenes

--- Modules/omnideconv/Autogenes_run.R
+++ Modules/omnideconv/Autogenes_run.R
@@ -30,7 +30,9 @@ rm(common)
 cellTypes <- phenData$cellType
 model <- omnideconv::build_model(C0, cellTypes, bulk_gene_expression = T, method = "autogenes")
 res <- omnideconv::deconvolute(T, signature = model, single_cell_object = C0, method = "autogenes")
+res[res < 10^-5] <- 0 #Convergence error tolerance = 10^-5
 res <- t(res/rowSums(res))
+
 if (filename_P != 'Modules/Psuedobulk/dummy_props.rds') res <- res[order(match(rownames(res), rownames(P))),]
 
 #Save and exit
