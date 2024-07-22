c5bdbe9170a25f1da1f87d7f2f07e9d9b2515ab7
A Vathrakokoili-Pournara <77834045+annavpo@users.noreply.github.com>
2024-07-22 16:35:18 +0100

    Update DWLS_run.R

--- Modules/DWLS/DWLS_run.R
+++ Modules/DWLS/DWLS_run.R
@@ -36,8 +36,8 @@ rm(common)
 
 #Preprocess
 message("Started running sig...")
-#Signature <- buildSignatureMatrixMAST(scdata = C0, id = phenData[,"cellType"], path = scratchDir, diff.cutoff = 0.5, pval.cutoff #= 0.01)
-load("Hrvatin_2018_new_scratch/Sig.RData")
+Signature <- buildSignatureMatrixMAST(scdata = C0, id = phenData[,"cellType"], path = scratchDir, diff.cutoff = 0.5, pval.cutoff = 0.01)
+#load("Hrvatin_2018_new_scratch/Sig.RData")
 Signature<-Sig
 
 #Get results and reorder the matrices for correspondence
